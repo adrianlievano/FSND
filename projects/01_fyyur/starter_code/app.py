4d45c72e4c7320111641e112a6d973e11729f640
adrianlievano <32432374+adrianlievano@users.noreply.github.com>
2020-06-04 17:59:18 -0400

    Add artist route by artist_id functionality

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -232,8 +232,6 @@ def show_venue(venue_id):
                 'upcoming_shows': upcoming_list,
                 'past_shows_count': pastShowsCount,
                 'upcoming_shows_count': upcomingShowsCount}
-
-  print(data)
   data1={
     "id": 1,
     "name": "The Musical Hop",
@@ -364,6 +362,7 @@ def create_venue_submission():
           db.session.close()
       return render_template('pages/home.html')
 
+#TODO
 @app.route('/venues/<venue_id>', methods=['DELETE'])
 def delete_venue(venue_id):
   # TODO: Complete this endpoint for taking a venue_id, and using
@@ -425,6 +424,47 @@ def search_artists():
 def show_artist(artist_id):
   # shows the venue page with the given venue_id
   # TODO: replace with real venue data from the venues table, using venue_id
+  artist = Artist.query.get(artist_id)
+  shows = Show.query.filter(Show.artist_id == artist_id).all()
+
+  now = datetime.utcnow()
+  past_shows_list = db.session.query(Show, Venue).join(Venue).filter(Show.artist_id == artist_id).filter(Show.start_time < now).all()
+  upcoming_shows_list = db.session.query(Show, Venue).join(Venue).filter(Show.artist_id == artist_id).filter(Show.start_time > now).all()
+
+  past_list = list()
+  for show in past_shows_list:
+     data = {'venue_id': show.Show.Venue.id,
+     'venue_name': show.Venue.name,
+     'venue_image_link': show.Venue.image_link,
+     'start_time': show.Show.start_time}
+     past_list.append(data)
+
+  upcoming_list = list()
+  for show in upcoming_shows_list:
+     data = {'venue_id': show.Show.Venue.id,
+     'venue_name': show.Venue.name,
+     'venue_image_link': show.Venue.image_link,
+     'start_time': show.Show.start_time}
+     upcoming_list.append(data)
+
+
+  datum = {
+    "id": artist.id,
+    "name": artist.name,
+    "genres": artist.genres,
+    "city": artist.city,
+    "state": artist.state,
+    "phone": artist.phone,
+    "website": artist.website_link,
+    "facebook_link": artist.facebook_link,
+    "seeking_venue": artist.seeking_venue,
+    "seeking_description": artist.seeking_description,
+    "image_link": artist.image_link,
+    "past_shows": past_list,
+    "upcoming_shows": upcoming_list,
+    "past_shows_count": len(past_list),
+    "upcoming_shows_count": len(upcoming_list)}
+
   data1={
     "id": 4,
     "name": "Guns N Petals",
@@ -496,7 +536,8 @@ def show_artist(artist_id):
     "past_shows_count": 0,
     "upcoming_shows_count": 3,
   }
-  data = list(filter(lambda d: d['id'] == artist_id, [data1, data2, data3]))[0]
+  #data = list(filter(lambda d: d['id'] == artist_id, [data1, data2, data3]))[0]
+  data = list(datum)
   return render_template('pages/show_artist.html', artist=data)
 
 #  Update
