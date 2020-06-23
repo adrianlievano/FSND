b3b6a6c9f2efb42f1d62fd2582d4c43d86c19883
adrianlievano <32432374+adrianlievano@users.noreply.github.com>
2020-06-22 22:03:16 -0400

    add get actors and get movies endpoint, add test case for both of those endpoints

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -2,11 +2,58 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from models import setup_db
 
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
   CORS(app)
+
+
+  @app.route('/actors', methods=['GET'])
+  def get_actors():
+    actors = Actors.query.all()
+    list_of_actors = []
+    for actor in actors:
+      list_of_actors.append(actor.format())
+    return jsonify({'success': True,
+                    'actors': list_of_actors})
+  
+  @app.route('/movies', methods=['GET'])
+  def get_actors():
+    movies = Movies.query.all()
+    list_of_movies = []
+    for movie in movies:
+      list_of_movies.append(movie.format())
+    return jsonify({'success': True,
+                    'actors': list_of_movies})
+
+  # Error Handling
+  @app.errorhandler(422)
+  def unprocessable(error):
+    return jsonify({
+      "success": False,
+      "error": 422,
+      "message": "unprocessable"
+    }), 422
+
+
+  @app.errorhandler(404)
+  def unprocessable(error):
+    return jsonify({
+      "success": False,
+      "error": 404,
+      "message": "Resource not found"
+    }), 404
+
+
+  @app.errorhandler(AuthError)
+  def auth_error(ex):
+    return jsonify({
+      "success": False,
+      "error": ex.status_code,
+      "message": ex.error['code']
+    }), 401
 
   return app
 
--- projects/capstone/starter/test_app.py
+++ projects/capstone/starter/test_app.py
@@ -1,1 +1,51 @@
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
 
+from flaskr import create_app
+from models import setup_db, Actors, Movies
+
+
+class AppTestCase(unittest.TestCase):
+    """This class represents the trivia test case"""
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = "tbd"
+        self.database_path = "postgres://{}/{}".format('localhost:5432',
+                                                       self.database_name)
+        setup_db(self.app, self.database_path)
+
+        # binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # create all tables
+            self.db.create_all()
+
+        sample_actor = {'name': 'Tom Cruise', 
+                        'Age': 34,
+                        'Gender': 'Male'}
+        
+        sample_movie = {'title': 'Mission Impossible',
+                        'release_date': '2020-05-02',
+                        'genre': 'Action'}
+
+    # Test get actors endpoint
+    def test_get_actors(self):
+        res = self.client().get('/actors')
+        data = json.loads(res.data)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['actors'])
+
+    # Test get movies endpoint
+    def test_get_actors(self):
+        res = self.client().get('/movies')
+        data = json.loads(res.data)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['movies'])
