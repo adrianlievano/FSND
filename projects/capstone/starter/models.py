1af91f9a3ddbf80907c0e58a95a012633a60331e
adrianlievano <32432374+adrianlievano@users.noreply.github.com>
2020-06-22 21:12:07 -0400

    build data models for Movies and Actors table

--- projects/capstone/starter/models.py
+++ projects/capstone/starter/models.py
@@ -1,1 +1,67 @@
+from sqlalchemy import Column, String, create_engine
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+database_path = os.environ['DATABASE_URL']
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+'''
+Movie
+Have title and release year
+'''
+class Movies(db.Model):  
+  __tablename__ = 'Movies'
+
+  id = Column(Integer, primary_key=True)
+  title = Column(String)
+  release_date = Column(DateTime)
+  genre = Column(String)
+
+  def __init__(self, title, release_date, genre):
+    self.title = title
+    self.release_date = release_date
+    self.genre = genre
+
+  def format(self):
+    return {
+      'id': self.id,
+      'title': self.title,
+      'release_date': self.release_date,
+      'genre': self.genre}
+
+'''
+Actor
+'''
+class Actors(db.Model):  
+  __tablename__ = 'Actors'
+
+  id = Column(Integer, primary_key=True)
+  name = Column(String)
+  age = Column(Integer)
+  gender = Column(String)
+
+  def __init__(self, name, age, gender):
+    self.name = name
+    self.age = age
+    self.gender = gender
+
+  def format(self):
+    return {
+      'id': self.id,
+      'name': self.name,
+      'age': self.age,
+      'gender': self.gender}
 
